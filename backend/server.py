4a659a2a45c12f0d23861dd5a58016646af4ae0a
rhutik03 <90514561+zappy0@users.noreply.github.com>
2023-02-05 07:15:51 +0530

    f

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import cv2  
+
 import datetime
   
+face_cascade = cv2.CascadeClassifier("D:/sem6/Hackathon-/backend/haarcascade_frontalface_default.xml") 
+  
+eye_cascade = cv2.CascadeClassifier("D:/sem6/Hackathon-/backend/haarcascade_eye_tree_eyeglasses.xml")  
+
 x = datetime.datetime.now()
   
 # Initializing flask app
@@ -20,7 +26,38 @@ def get_time():
     "materialPref": "001",
     "pastAnalysis":"011"
         }
+
+@app.route('/checkawr')
+def checkawr(cap):
+    
+    # image
+    img = cv2.imread('D:/sem6/SPIT_H2023/SPIT_H2023/backend/noface.jpg')
+    # img = cv2.imread((JSON.parse(cap))[0])
+    # convert to gray scale of each frames 
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
   
+    # Detects faces of different sizes in the input image 
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5) 
+    if type(faces) is tuple:
+        return false
+    
+    for (x,y,w,h) in faces: 
+        # To draw a rectangle in a face  
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)  
+        roi_gray = gray[y:y+h, x:x+w] 
+        roi_color = img[y:y+h, x:x+w] 
+  
+        # Detects eyes of different sizes in the input image 
+        eyes = eye_cascade.detectMultiScale(roi_gray)  
+        if type(eyes) is tuple :
+            return false
+    
+    # De-allocate any associated memory usage 
+    cv2.destroyAllWindows()  
+    
+    return True
+    
+    
       
 # Running app
 if __name__ == '__main__':
